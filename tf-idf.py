a6a4cbcd696f1ccf8f3ec94d05d0800072d656fe
Ks-ai-max <145446580+Ks-ai-max@users.noreply.github.com>
2023-12-02 23:02:22 +0300

    Add files via upload
    tf-idf model for an article about nlp

---
+++ tf-idf.py
@@ -0,0 +1,45 @@
+import nltk
+import numpy as np
+
+import bs4 as bs
+import urllib.request
+import re
+
+raw_html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Natural_language_processing')
+raw_html = raw_html.read()
+
+article_html = bs.BeautifulSoup(raw_html, 'lxml')
+
+article_paragraphs = article_html.find_all('p')
+
+article_text = ''
+
+for para in article_paragraphs:
+    article_text += para.text
+
+corpus = nltk.sent_tokenize(article_text)
+
+for i in range(len(corpus )):
+    corpus [i] = corpus [i].lower()
+    corpus [i] = re.sub(r'\W',' ',corpus [i])
+    corpus [i] = re.sub(r'\s+',' ',corpus [i])
+
+wordfreq = {}
+for sentence in corpus:
+    tokens = nltk.word_tokenize(sentence)
+    for token in tokens:
+        if token not in wordfreq.keys():
+            wordfreq[token] = 1
+        else:
+            wordfreq[token] += 1
+
+import heapq
+most_freq = heapq.nlargest(200, wordfreq, key=wordfreq.get)
+word_idf_values = {}
+for token in most_freq:
+    doc_containing_word = 0
+    for document in corpus:
+        if token in nltk.word_tokenize(document):
+            doc_containing_word += 1
+    word_idf_values[token] = np.log(len(corpus)/(1 + doc_containing_word))
+    
