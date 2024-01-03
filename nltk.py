fdb3bf628c64dcd743fc312abcd989d5ca3ff897
Ks-ai-max <145446580+Ks-ai-max@users.noreply.github.com>
2024-01-03 18:45:12 +0300

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -3,6 +3,8 @@ from nltk import word_tokenize, sent_tokenize
 sent = str(input())
 print(word_tokenize(sent))
 print(sent_tokenize(sent))
+
+
 # Removing stop-words
 from nltk.corpus import stopwords
 stop_words = stopwords.words('russian')
@@ -15,26 +17,28 @@ for word in token:
         cleaned_token.append(word)
 print(token)
 print(cleaned_token)
+
+
 # Stemming
 from nltk.stem import SnowballStemmer
 stemmer = SnowballStemmer('english')
 words = ['processing', 'processor', 'procession', 'processed', 'inprocess']
 stemmed = [stemmer.stem(word) for word in words]
 print(stemmed)
+
+
 # Lemmatization 
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
-
 nltk.download('wordnet')
-
 lemmatizer = WordNetLemmatizer()
-
 word = "running"
 pos_tag = wordnet.VERB
-
 lemma = lemmatizer.lemmatize(word, pos=pos_tag)
 print(lemma)
+
+
 # POS tagging 
 from nltk import pos_tag
 from nltk import word_tokenize
